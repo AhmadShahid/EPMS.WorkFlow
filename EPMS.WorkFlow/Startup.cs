a1011e30cad883b9b641dcb3bf57f18786d1b8cf
Shahid <shahidahmad527@gmail.com>
2017-10-18 16:50:02 +0500

    initial commit

---
+++ EPMS.WorkFlow/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EPMS.WorkFlow.Startup))]
+namespace EPMS.WorkFlow
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
