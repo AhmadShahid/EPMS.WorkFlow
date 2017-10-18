cd6c2733fafdcc67971cc7bac5c1bc4001218245
Shahid <shahidahmad527@gmail.com>
2017-10-18 16:56:57 +0500

    add mvc project

---
+++ EPMS.WorkFlow.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EPMS.WorkFlow.Web.MvcApplication" Language="C#" %>
---
+++ EPMS.WorkFlow.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EPMS.WorkFlow.Web.Startup))]
+namespace EPMS.WorkFlow.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
